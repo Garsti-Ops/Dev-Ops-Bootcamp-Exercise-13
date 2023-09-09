68fd4a6337a898bcb5aa422cf0d8e6b0eabb3790
Christoph Heider <christoph.heider@vema-eg.de>
2023-09-09 11:33:02 +0200

    Exercise 2 completed

--- exercise2.py
+++ exercise2.py
@@ -13,3 +13,24 @@ print(employee)
 
 for key in employee.keys():
     print(f'Key: {key} Value: {employee[key]}')
+
+dict_one = {'a': 100, 'b': 400}
+dict_two = {'x': 300, 'y': 200}
+whole_dict = {}
+
+for key in dict_one.keys():
+    whole_dict[key] = dict_one[key]
+
+for key in dict_two.keys():
+    whole_dict[key] = dict_two[key]
+
+print(whole_dict)
+
+sum = 0
+for key in whole_dict.keys():
+    sum += whole_dict[key]
+
+print(sum)
+
+sorted_list = sorted(whole_dict.items(), key=lambda x: x[1])
+print(sorted_list[0], sorted_list[len(sorted_list) - 1])
